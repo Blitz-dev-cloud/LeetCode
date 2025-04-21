ca7d7ac02faade780b36affb01c0123fc4fb9dab
_ichigo_0 <sounak1629@gmail.com>
2025-04-21 23:06:33 +0530

    Time: 1 ms (24.33%) | Memory: 9.4 MB (33.10%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     //blitz
-    /*int f(int i, int j, int m, int n, vector<vector<int>> &dp){
+    int f(int i, int j, int m, int n, vector<vector<int>> &dp){
         if(i == m - 1 && j == n - 1) return 1;
         if(dp[i][j] != -1) return dp[i][j];
         int d = 0, r = 0;
         if(i < m - 1) d = f(i + 1, j, m, n, dp);
         if(j < n - 1) r = f(i, j + 1, m, n, dp);
         return dp[i][j] = d + r;
-    } -> by memoization*/
+    } /*-> by memoization*/
+    int uniquePaths(int m, int n){
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        return f(0, 0, m, n, dp);
+    }
+    /*
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int> (n, 0));
         for( int i = 0 ; i < m ; i++ ) dp[i][0] = 1;
