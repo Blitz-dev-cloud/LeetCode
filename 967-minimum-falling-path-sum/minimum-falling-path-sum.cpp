cb4dfb260cdb56f42c66c6ec6888ead37859a449
_ichigo_0 <sounak1629@gmail.com>
2025-04-22 00:48:17 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (79.53%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -13,17 +13,20 @@ public:
     }*/
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp(n, vector<int> (n , 0));
-        for( int i = 0 ; i < n ; i++ ) dp[n - 1][i] = matrix[n - 1][i];
+        //vector<vector<int>> dp(n, vector<int> (n , 0));
+        vector<int> front(n, 0);
+        for( int i = 0 ; i < n ; i++ ) front[i] = matrix[n - 1][i];
         int dl = 0, d = 0, dr = 0;
         for( int i = n - 2 ; i >= 0 ; i-- ){
+            vector<int> curr(n, 0);
             for( int j = 0 ; j < n ; j++ ){
-                dl = (j > 0) ? matrix[i][j] + dp[i + 1][j - 1] : INT_MAX;
-                d = matrix[i][j] + dp[i + 1][j];
-                dr = (j < n - 1) ? matrix[i][j] + dp[i + 1][j + 1] : INT_MAX;
-                dp[i][j] = min({dl, d, dr});
+                dl = (j > 0) ? matrix[i][j] + front[j - 1] : INT_MAX;
+                d = matrix[i][j] + front[j];
+                dr = (j < n - 1) ? matrix[i][j] + front[j + 1] : INT_MAX;
+                curr[j] = min({dl, d, dr});
             }
+            front = curr;
         }
-        return *min_element(dp[0].begin(), dp[0].end());
+        return *min_element(front.begin(), front.end());
     }
 };
