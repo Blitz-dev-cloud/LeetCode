6f95d964cac83ca9d0df293da470531ad67f085f
_ichigo_0 <sounak1629@gmail.com>
2025-04-26 19:07:24 +0530

    Time: 26 ms (63.19%) | Memory: 27.4 MB (50.26%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     //blitz
-    int search(string text1, string text2, int m, int n, vector<vector<int>> &lcs){
+    /*int f(int i, int j, string p, string t, vector<vector<int>> &dp){
+        if(i == 0 || j == 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(p[i - 1] == t[j - 1]) return dp[i][j] = 1 + f(i - 1, j - 1, p, t, dp);
+        return dp[i][j] = 0 + max(f(i - 1, j, p, t, dp), f(i, j - 1, p, t, dp));
+    }*/
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.length(), n = text2.length();
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
         for( int i = 1 ; i <= m ; i++ ){
             for( int j = 1 ; j <= n ; j++ ){
                 if(text1[i - 1] == text2[j - 1]){
-                    lcs[i][j] = lcs[i - 1][j - 1] + 1;
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else{
-                    lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1]);
+                    dp[i][j] = 0 + max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        return lcs[m][n];
-    }
-    int longestCommonSubsequence(string text1, string text2) {
-        int m = text1.length(), n = text2.length();
-        vector<vector<int>> lcs(m + 1, vector<int>(n + 1, 0));
-        return search(text1, text2, m, n, lcs);
+        return dp[m][n];
     }
 };
