44453b38a35cc761e189c2a849a66eda04f04cdb
_ichigo_0 <sounak1629@gmail.com>
2025-07-12 17:56:27 +0530

    Time: 7 ms (7.23%) | Memory: 17.4 MB (14.61%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,45 +1,47 @@
 class Solution {
 public:
-    //blitz
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        queue <pair<pair<int, int>, int>> q;
-        vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<pair<pair<int, int>, int>> q;
+        vector<vector <int>> visited(n, vector<int> (m, 0));
+        int countFresh = 0;
+
         for( int i = 0 ; i < n ; i++ ){
             for( int j = 0 ; j < m ; j++ ){
-                if(grid[i][j] == 2){
+                if(grid[i][j] == 2) {
                     q.push({{i, j}, 0});
-                    vis[i][j] = 2;
+                    visited[i][j] = 1;
                 }
+                if(grid[i][j] == 1) countFresh++;
             }
         }
 
-        int time = 0;
-        int drow[] = {-1, 0, 1, 0}, dcol[] = {0, 1, 0, -1};
+        int totalMinutes = 0;
+        vector<int> delRow = {-1, 0, 1, 0};
+        vector<int> delColumn = {0, 1, 0, -1};
+        int count = 0;
         while(!q.empty()){
             int r = q.front().first.first;
             int c = q.front().first.second;
             int t = q.front().second;
-            time = max(time, t);
+            totalMinutes = max(totalMinutes, t);
             q.pop();
 
             for( int i = 0 ; i < 4 ; i++ ){
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1){
-                    q.push({{nrow, ncol}, t + 1});
-                    vis[nrow][ncol] = 2;
+                int nRow = r + delRow[i];
+                int nCol = c + delColumn[i];
+                if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m
+                && !visited[nRow][nCol] && grid[nRow][nCol] == 1){
+                    q.push({{nRow, nCol}, t + 1});
+                    visited[nRow][nCol] = 1;
+                    count++;
                 }
             }
         }
 
-        for( int i = 0 ; i < n ; i++ ){
-            for( int j = 0 ; j < m ; j++ ){
-                if(grid[i][j] == 1 && vis[i][j] != 2) return -1;
-            }
-        }
+        if(count != countFresh) return -1;
 
-        return time;
+        return totalMinutes;
     }
 };
