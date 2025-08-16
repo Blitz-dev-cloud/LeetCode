9cedd251d474cd9baab6400fed41d429b802341f
_ichigo_0 <sounak1629@gmail.com>
2025-08-16 15:50:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (94.22%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        vector<int> arr;
+        /*vector<int> arr;
         while(num > 0) {
             int r = num % 10;
             arr.push_back(r);
@@ -17,6 +17,13 @@ public:
         for( int i = 0 ; i < arr.size() ; i++ ) {
             n += arr[i] * pow(10, i);
         }
-        return n;
+        return n;*/
+        int n = num, pos = 1, change = 0;
+        while( n > 0 ) {
+            if(n % 10 == 6) change = 3 * pos;
+            n /= 10;
+            pos *= 10;
+        }
+        return num + change;
     }
 };
