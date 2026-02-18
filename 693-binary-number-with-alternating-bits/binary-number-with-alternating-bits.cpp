07c85a5bda05f0573705fbaada18729d3b0d4297
sounak <sounak1629@gmail.com>
2026-02-18 08:34:15 -0500

    Time: 0 ms (100.00%) | Memory: 7.7 MB (91.43%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        string bin = bitset<32> (n).to_string();
-        bin = bin.substr(bin.find('1'));
-        cout << bin;
-
-        bool prevHigh = (bin[0] == '1' ? true : false);
-        
-        for( int i = 1 ; i < bin.size() ; i++ ) {
-            if((prevHigh && bin[i] == '1') || (!prevHigh && bin[i] == '0')) return false;
-            prevHigh = (bin[i] == '1' ? true : false);
-        }
-
-        return true;
+        long long x = n ^ (n >> 1);
+        return (x & (x + 1)) == 0;
     }
 };
