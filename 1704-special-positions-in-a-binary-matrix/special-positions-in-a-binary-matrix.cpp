01d7d518e692f0c29c613fbb767b9004928309f9
sounak <sounak1629@gmail.com>
2026-03-04 11:19:03 -0500

    Time: 0 ms (100.00%) | Memory: 17 MB (41.15%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -4,8 +4,7 @@ public:
         int m = mat.size();
         int n = mat[0].size();
 
-        unordered_map<int, int> rows;
-        unordered_map<int, int> cols;
+        vector<int> rows(m, 0), cols(n, 0);
 
         for( int i = 0 ; i < m ; i++ ) {
             for( int j = 0 ; j < n ; j++ ) {
