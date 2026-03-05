5b04d042fb89131ca6690226b5b8c19975fa9f50
sounak <sounak1629@gmail.com>
2026-03-04 21:58:56 -0500

    Time: 0 ms (100.00%) | Memory: 9.3 MB (76.20%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -2,24 +2,14 @@ class Solution {
 public:
     int minOperations(string s) {
         int n = s.size();
-
-        string alt_s_1 = "", alt_s_2 = "";
-        bool flag = true;
-        for( int i = 0 ; i < n ; i++ ) {
-            if(flag) {
-                alt_s_1 += "0";
-                alt_s_2 += "1";
-            } else {
-                alt_s_1 += "1";
-                alt_s_2 += "0"; 
-            }
-            flag = !flag;
-        }
-
         int op1 = 0, op2 = 0;
+
         for( int i = 0 ; i < n ; i++ ) {
-            if(s[i] == alt_s_1[i]) op1++;
-            if(s[i] == alt_s_2[i]) op2++;
+            char expected_1 = (i % 2 == 0) ? '0' : '1';
+            char expected_2 = (i % 2 == 0) ? '1' : '0';
+
+            if(s[i] == expected_1) op1++;
+            if(s[i] == expected_2) op2++;
         }
 
         return min(op1, op2);
