4661112341819614c82c131fbde5c93127811a7c
sounak <sounak1629@gmail.com>
2026-05-17 16:17:13 +0530

    Time: 1 ms (17.80%) | Memory: 8.8 MB (12.94%) - LeetSync

---
+++ 4305-check-adjacent-digit-differences/check-adjacent-digit-differences.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAdjacentDiffAtMostTwo(string s) {
+        int n = s.size();
+        for( int i = 1 ; i < n ; i++ ) {
+            int d1 = s[i - 1] - '0';
+            int d2 = s[i] - '0';
+            if(abs(d2 - d1) > 2) return false;
+        }
+        return true;
+    }
+};
