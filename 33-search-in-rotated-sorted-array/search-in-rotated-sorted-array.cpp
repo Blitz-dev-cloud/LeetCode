3fde28022f18b028f61463f4f7098debac5a35d9
sounak <sounak1629@gmail.com>
2026-05-22 16:43:35 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (66.43%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,23 +1,23 @@
 class Solution {
+private:
+    int searchTarget(int l, int r, vector<int> &nums, int target) {
+        if(l <= r) {
+            int mid = l + (r - l) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[l] <= nums[mid]) {
+                if(nums[l] <= target && target <= nums[mid]) return searchTarget(l, mid - 1, nums, target);
+                else return searchTarget(mid + 1, r, nums, target);
+            }
+            else if(nums[mid] <= nums[r]) {
+                if(nums[mid] <= target && target <= nums[r]) return searchTarget(mid + 1, r, nums, target);
+                else return searchTarget(l, mid - 1, nums, target);
+            }
+        }
+        return -1;
+    }
 public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
-        int l = 0, r = n - 1;
-
-        while(l <= r) {
-            int mid = l + (r - l) / 2;
-
-            if(nums[mid] == target) return mid;
-
-            if(nums[l] <= nums[mid]) {
-                if(nums[l] <= target && target < nums[mid]) r = mid - 1;
-                else l = mid + 1;
-            } else {
-                if(target > nums[mid] && nums[r] >= target) l = mid + 1;
-                else r = mid - 1;
-            }
-        }
-
-        return -1;
+        return searchTarget(0, n - 1, nums, target);
     }
 };
