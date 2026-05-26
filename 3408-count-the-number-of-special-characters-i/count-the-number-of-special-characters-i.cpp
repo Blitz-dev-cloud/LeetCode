840e79810b8f14bc5b3746bd8e6323db84b3a4cc
sounak <sounak1629@gmail.com>
2026-05-26 06:13:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (19.49%) - LeetSync

---
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        unordered_set<char> s;
+        for( char w : word ) {
+            if(isupper(w)) s.insert(w);
+        }
+
+        int result = 0;
+        for( char w : word ) {
+            if(islower(w) && !(s.find(w) != s.end())) {
+                s.insert(w);
+                char W = toupper(w);
+                if(s.find(W) != s.end()) result++;
+            }
+        }
+
+        return result;
+    }
+};
