c0f0f7fe07c3e6fa97cad7c11e447d7f362b4d6d
sounak <sounak1629@gmail.com>
2026-06-09 17:23:30 +0530

    Time: 7 ms (40.39%) | Memory: 15.2 MB (46.18%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,8 +1,7 @@
 class Solution {
 private:
     vector<vector<int>> result;
-    void dfs(int node, int n, vector<vector<int>> &graph, vector<bool> &visited, vector<int> &path) {
-        visited[node] = true;
+    void dfs(int node, int n, vector<vector<int>> &graph, vector<int> &path) {
         path.push_back(node);
 
         if(node == n - 1) {
@@ -10,22 +9,18 @@ private:
         }
 
         for( int adjNode : graph[node] ) {
-            if(!visited[adjNode]) {
-                dfs(adjNode, n, graph, visited, path);
-            }
+            dfs(adjNode, n, graph, path);
         }
 
         path.pop_back();
-        visited[node] = false;
     }
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         int n = graph.size();
 
-        vector<bool> visited(n, false);
         vector<int> path;
 
-        dfs(0, n, graph, visited, path);
+        dfs(0, n, graph, path);
 
         return result;
     }
