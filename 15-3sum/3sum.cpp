c8909e709ed6dec8fb53f568f11183fdcb18818d
sounak <sounak1629@gmail.com>
2026-07-02 03:14:48 +0530

    Time: 43 ms (84.23%) | Memory: 29.1 MB (47.27%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,41 +1,35 @@
 class Solution {
-private:
-    vector<vector<int>> kSumHelper(vector<int> &nums, int target, int k, int start) {
-        vector<vector<int>> res;
-        int n = nums.size();
-        
-        if(k == 2) {
-            int l = start, r = n - 1;
-            while( l < r ) {
-                int sum = nums[l] + nums[r];
-                if(sum == target) {
-                    res.push_back({nums[l], nums[r]});
-                    while( l < r && nums[l] == nums[l + 1] ) l++;
-                    while( l < r && nums[r] == nums[r - 1] ) r--;
-                    l++;
-                    r--;
-                } else if(sum < target) l++;
-                else r--;
-            }
-            return res;
-        }
-
-        for( int i = start ; i < n - k + 1 ; i++ ) {
-            if(i > start && nums[i] == nums[i - 1]) continue;
-
-            vector<vector<int>> temp = kSumHelper(nums, target - nums[i], k - 1, i + 1);
-
-            for(auto &vec : temp) {
-                vec.insert(vec.begin(), nums[i]);
-                res.push_back(vec);
-            }
-        }
-
-        return res;
-    }
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        return kSumHelper(nums, 0, 3, 0);
+
+        vector<vector<int>> result;
+
+        for( int i = 0 ; i < n  - 2 ; i++ ) {
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int l = i + 1;
+            int r = n - 1;
+
+            while(l < r) {
+                int sum = nums[l] + nums[i] + nums[r];
+
+                if(sum == 0) {
+                    result.push_back({nums[l], nums[i], nums[r]});
+                    l++;
+                    r--;
+
+                    while(l < r && nums[l] == nums[l - 1]) l++;
+                    while(l < r && nums[r] == nums[r + 1]) r--;
+                } else if(sum < 0) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+
+        return result;
     }
 };
