f6b4c1271fa48187f311e429b2e869489bc8b311
sounak <sounak1629@gmail.com>
2026-07-08 02:13:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (80.06%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num / 10 == 0) return num;
+
+        int sum = 0;
+        while(num > 0) {
+            int r = num % 10;
+            sum += r;
+            num /= 10;
+        }
+
+        return addDigits(sum);
+    }
+};
