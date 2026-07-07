e9d645d0dbd9904c3e2acf80ea1c5f968efff3a5
sounak <sounak1629@gmail.com>
2026-07-08 01:52:20 +0530

    Time: 20 ms (74.43%) | Memory: 18 MB (73.51%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    //blitz
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<int> dp(amount + 1, 1e9);
+        vector<int> dp(amount + 1, INT_MAX);
         dp[0] = 0;
-        
-        for( int i = 0 ; i < n ; i++ ){
-            for( int j = coins[i] ; j <= amount ; j++ ){
-                if(dp[j - coins[i]] != 1e9){
-                    dp[j] = min(dp[j], 1 + dp[j - coins[i]]);
+
+        for( int i = 1 ; i <= amount ; i++ ) {
+            for( int coin : coins ) {
+                if(coin <= i && dp[i - coin] != INT_MAX) {
+                    dp[i] = min(dp[i], 1 + dp[i - coin]);
                 }
             }
         }
 
-        return dp[amount] != 1e9 ? dp[amount] : -1;
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
 };
