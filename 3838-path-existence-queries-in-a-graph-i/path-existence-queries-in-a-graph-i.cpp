1b411c8989f1ca80e798d01d71b1fb530e1e8ab4
sounak <sounak1629@gmail.com>
2026-07-10 17:04:47 +0530

    Time: 55 ms (28.18%) | Memory: 228.8 MB (55.25%) - LeetSync

---
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -0,0 +1,56 @@
+class DSU {
+    public:
+        vector<int> size, parent;
+
+        DSU(int n) {
+            size.resize(n, 1);
+            parent.resize(n);
+            for( int i = 0 ; i < n ; i++ ) parent[i] = i;
+        }
+
+        int findParent(int u) {
+            if(parent[u] == u) return u;
+
+            return parent[u] = findParent(parent[u]);
+        }
+
+        void unionBySize(int u, int v) {
+            int p_u = findParent(u);
+            int p_v = findParent(v);
+
+            if(p_u == p_v) return;
+
+            if(size[p_u] < size[p_v]) {
+                parent[p_u] = p_v;
+                size[p_v] += size[p_u];
+            } else {
+                parent[p_v] = p_u;
+                size[p_u] += size[p_v];
+            }
+        }
+};
+
+class Solution {
+public:
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        DSU dsu(n);
+
+        sort(nums.begin(), nums.end());
+
+        for( int i = 1 ; i < n ; i++ ) {
+            if(abs(nums[i - 1] - nums[i]) <= maxDiff) dsu.unionBySize(i - 1, i);
+        }
+
+        vector<bool> result;
+
+        for( int i = 0 ; i < queries.size() ; i++ ) {
+            int p_u = dsu.findParent(queries[i][0]);
+            int p_v = dsu.findParent(queries[i][1]);
+
+            if(p_u == p_v) result.push_back(true);
+            else result.push_back(false);
+        }
+
+        return result;
+    }
+};
