3e1016440e68fb534e52fe045f4a9d256503fb6e
sounak <sounak1629@gmail.com>
2026-07-13 00:46:45 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (85.04%) - LeetSync

---
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        for( int i = 0 ; i < n ; i++ ) {
+            if(nums[i] == target) {
+                vector<int> result;
+                while(i < n && nums[i] == target) {
+                    result.push_back(i);
+                    i++;
+                }
+                return result;
+            }
+        }
+
+        return {};
+    }
+};
