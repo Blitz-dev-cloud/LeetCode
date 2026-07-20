d5d140283e4e2168fe20ed3976c76ea27fc98a97
sounak <sounak1629@gmail.com>
2026-07-20 15:04:58 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (91.78%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.cpp
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -8,24 +8,17 @@ public:
 
         k %= N;
 
-        vector<int> result(N);
+        vector<vector<int>> result(m, vector<int> (n));
 
         for( int i = 0 ; i < m ; i++ ) {
             for( int j = 0 ; j < n ; j++ ) {
-                result[i * n + j] = grid[i][j];
+                int idx = i * n + j;
+                int newIdx = (idx + k) % N;
+
+                result[newIdx / n][newIdx % n] = grid[i][j];
             }
         }
 
-        reverse(result.begin(), result.end());
-        reverse(result.begin(), result.begin() + k);
-        reverse(result.begin() + k, result.end());
-
-        for( int i = 0 ; i < m ; i++ ) {
-            for( int j = 0 ; j < n ; j++ ) {
-                grid[i][j] = result[i * n + j];
-            }
-        }
-
-        return grid;
+        return result;
     }
 };
