deeb59c37317af2749d55db8d28a3b0516d0d9b6
sounak <sounak1629@gmail.com>
2026-07-31 15:15:18 +0530

    Time: 14 ms (37.38%) | Memory: 26 MB (55.63%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<pair<char, int>> freq;
+        vector<int> visited(26, -1);
+
+        int idx = 0;
+
+        for( char x : word ) {
+            if(visited[x - 'a'] == -1) {
+                freq.push_back({x, 1});
+                visited[x - 'a'] = idx++;
+            } else {
+                int i = visited[x - 'a'];
+                freq[i].second++;
+            }
+        }
+
+        sort(freq.begin(), freq.end(), [](const pair<int, int> &a, const pair<int, int> &b) {
+            return a.second > b.second;
+        });
+
+        int factor = 0;
+        int sum = 0;
+
+        for( int i = 0 ; i < freq.size() ; i++ ) {
+            if(i % 8 == 0) factor++;
+            sum += factor * freq[i].second;
+        }
+
+        return sum;
+    }
+};
