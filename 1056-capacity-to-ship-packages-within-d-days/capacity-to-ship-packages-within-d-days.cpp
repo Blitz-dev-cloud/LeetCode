5093a2ea69d9a379afdffe5fc46e21a6945c4050
sounak <sounak1629@gmail.com>
2026-08-10 07:52:06 +0530

    Time: 13 ms (40.08%) | Memory: 35.1 MB (20.31%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,36 +1,35 @@
 class Solution {
 private:
-    bool canShip(vector<int> &weights, int days, int cap) {
-        int requiredDays = 1;
-        int load = 0;
-
-        for( int weight : weights ) {
-            if(load + weight > cap) {
-                requiredDays++;
-                load = weight;
+    bool canShip(int cap, int days, vector<int> &weights) {
+        int curr = 0;
+        int used = 1;
+        for( int w : weights ) {
+            if(curr + w <= cap) {
+                curr += w;
             } else {
-                load += weight;
+                used++;
+                curr = w;
             }
         }
-
-        return requiredDays <= days;
+        return used <= days;
     }
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int n = weights.size();
         int l = *max_element(weights.begin(), weights.end());
         int r = accumulate(weights.begin(), weights.end(), 0);
 
+        int ans = 1;
+
         while(l <= r) {
             int mid = l + (r - l) / 2;
-
-            if(canShip(weights, days, mid)) {
+            if(canShip(mid, days, weights)) {
+                ans = mid;
                 r = mid - 1;
             } else {
                 l = mid + 1;
             }
         }
 
-        return l;
+        return ans;
     }
 };
