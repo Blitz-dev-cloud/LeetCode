a512d6739c7e6c342f77294c7495855a07434920
sounak <sounak1629@gmail.com>
2026-08-10 12:23:44 +0530

    Time: 9 ms (88.53%) | Memory: 62.2 MB (52.72%) - LeetSync

---
+++ 659-split-array-into-consecutive-subsequences/split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        unordered_map<int, int> freq, need;
+
+        for( int x : nums ) freq[x]++;
+
+        for( int x : nums ) {
+            if(freq[x] == 0) continue;
+
+            if(need[x] > 0) {
+                need[x]--;
+                freq[x]--;
+                need[x + 1]++;
+            } else if(freq[x + 1] > 0 && freq[x + 2] > 0) {
+                freq[x]--;
+                freq[x + 1]--;
+                freq[x + 2]--;
+
+                need[x + 3]++;
+            } else return false;
+        }
+
+        return true;
+    }
+};
