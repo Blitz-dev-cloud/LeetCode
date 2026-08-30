7cb1c0d111a0a08e9c772fde37e4121ecdc636d9
sounak <sounak1629@gmail.com>
2026-08-30 23:58:09 +0530

    Time: 86 ms (46.61%) | Memory: 44.2 MB (83.56%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,39 +1,48 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int, int>>> graph(n + 1);
+        int v = times.size();
 
-        for( auto &t : times ) { 
-            int u = t[0], v = t[1], w = t[2];
-            graph[u].push_back({v, w});
+        vector<vector<pair<int, int>>> adj(n + 1);
+        for( int i = 0 ; i < v ; i++ ) {
+            int u = times[i][0];
+            int v = times[i][1];
+            int w = times[i][2];
+
+            adj[u].push_back({v, w});
         }
-
-        vector<int> dist(n + 1, INT_MAX);
-        dist[k] = 0; 
 
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         pq.push({0, k});
 
+        vector<int> dist(n + 1, INT_MAX);
+        dist[k] = 0;
+
         while(!pq.empty()) {
-            auto [time, u] = pq.top();
+            auto p = pq.top();
             pq.pop();
 
-            if(time > dist[u]) continue;
+            int time = p.first;
+            int node = p.second;
 
-            for( auto &[v, w] : graph[u] ) {
-                if(time + w < dist[v]) {
-                    dist[v] = time + w;
-                    pq.push({dist[v], v});
+            // if(allVisited(visited)) return time;
+
+            for( auto& q : adj[node] ) {
+                int nextNode = q.first;
+                int t = q.second;
+
+                if(dist[node] + t < dist[nextNode]) {
+                    dist[nextNode] = dist[node] + t;
+                    pq.push({dist[nextNode], nextNode});
+                    // visited[nextNode] = true;
                 }
             }
         }
 
-        int ans = 0;
         for( int i = 1 ; i <= n ; i++ ) {
             if(dist[i] == INT_MAX) return -1;
-            ans = max(ans, dist[i]);
         }
 
-        return ans;
+        return *max_element(dist.begin() + 1, dist.end());
     }
 };
