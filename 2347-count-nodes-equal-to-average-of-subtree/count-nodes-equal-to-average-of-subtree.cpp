d11c629a80e4a2996b4e9ab47967728a10bc32e8
sounak <sounak1629@gmail.com>
2026-09-10 17:02:15 +0530

    Time: 7 ms (54.96%) | Memory: 15.8 MB (64.72%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -11,24 +11,23 @@
  */
 class Solution {
 private:
-    unordered_map<TreeNode* , pair<int, int>> store;
+    // unordered_map<TreeNode* , pair<int, int>> store;
     int ans = 0;
 
-    void dfs(TreeNode* root) {
+    pair<int, int> dfs(TreeNode* root) {
         if(!root->left && !root->right) {
             ans++;
-            store[root] = {root->val, 1};
-            return;
+            return {root->val, 1};
         }
-
-        if(root->left) dfs(root->left);
-        if(root->right) dfs(root->right);
 
         pair p1 = {0, 0};
         pair p2 = {0, 0};
 
-        if(store.count(root->left)) p1 = store[root->left];
-        if(store.count(root->right)) p2 = store[root->right];
+        if(root->left) p1 = dfs(root->left);
+        if(root->right) p2 = dfs(root->right);
+
+        // if(store.count(root->left)) p1 = store[root->left];
+        // if(store.count(root->right)) p2 = store[root->right];
 
         int v1 = p1.first, c1 = p1.second;
         int v2 = p2.first, c2 = p2.second;
@@ -37,7 +36,7 @@ private:
 
         if(avg == root->val) ans++;
 
-        store[root] = {root->val + v1 + v2, c1 + c2 + 1};
+        return {root->val + v1 + v2, c1 + c2 + 1};
     }
 public:
     int averageOfSubtree(TreeNode* root) {
